db529b1b7ce17ad673b606c160087c51d456d16c
wadosmangprail@gmail.com <wadosmangprail@gmail.com>
2020-08-17 02:18:48 +0100

    update

--- src/components/SearchIn.js
+++ src/components/SearchIn.js
@@ -4,6 +4,7 @@ const SearchIn = ({searchChange}) => {
     return (
     <div className="ma3">
         <input
+        area-aria-label='Search Robots'
         onChange={searchChange}
         className="pa2 ba b--black bg-light-blue grow br2 shadow-5"
         type='seach' 
